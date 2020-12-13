8ccd335b38f8c379bbfa03cd217ac02e26b42dd5
Chris Mumford <cmumford@cmumford.com>
2020-12-12 21:09:32 -0800

    More WiFi work.

--- main/wifi.cc
+++ main/wifi.cc
@@ -9,14 +9,86 @@ namespace {
 constexpr char TAG[] = "wifi";
 constexpr size_t kMaxSSIDLen = 31;
 constexpr size_t kMaxKeyLen = 63;
+constexpr int kMaxNumConnectRetry = 10;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
 }  // namespace
 
-WiFi::WiFi() = default;
+void WiFi::EventHandler(esp_event_base_t event_base,
+                        int32_t event_id,
+                        void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (retry_num_ < kMaxNumConnectRetry) {
+      esp_wifi_connect();
+      retry_num_++;
+      ESP_LOGI(TAG, "retry to connect to the AP");
+    } else {
+      xEventGroupSetBits(wifi_event_group_, WIFI_FAIL_BIT);
+    }
+    ESP_LOGI(TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    retry_num_ = 0;
+    xEventGroupSetBits(wifi_event_group_, WIFI_CONNECTED_BIT);
+  }
+}
 
-WiFi::~WiFi() = default;
+// static
+void WiFi::EventHandler(void* arg,
+                        esp_event_base_t event_base,
+                        int32_t event_id,
+                        void* event_data) {
+  static_cast<WiFi*>(arg)->EventHandler(event_base, event_id, event_data);
+}
+
+WiFi::WiFi()
+    : wifi_event_group_(nullptr),
+      instance_any_id_(nullptr),
+      instance_got_ip_(nullptr),
+      retry_num_(0) {}
+
+WiFi::~WiFi() {
+  /* The event will not be processed after unregister */
+  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip_));
+  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id_));
+  vEventGroupDelete(wifi_event_group_);
+}
+
+esp_err_t WiFi::Inititialize() {
+  wifi_event_group_ = xEventGroupCreate();
+
+  esp_err_t err = esp_netif_init();
+  if (err != ESP_OK)
+    return err;
+
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  err = esp_wifi_init(&cfg);
+  if (err != ESP_OK)
+    return err;
+
+  err = esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &EventHandler, this, &instance_any_id_);
+  if (err != ESP_OK)
+    return err;
+  err = esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &EventHandler, this, &instance_got_ip_);
+
+  return err;
+}
 
 esp_err_t WiFi::Connect(const std::string& ssid, const std::string& key) {
   ESP_LOGI(TAG, "Connecting to WiFi network: \"%s\"", ssid.c_str());
+
+  retry_num_ = 0;
 
   if (ssid.length() > kMaxSSIDLen || key.length() > kMaxKeyLen)
     return ESP_ERR_INVALID_SIZE;
--- main/wifi.h
+++ main/wifi.h
@@ -3,10 +3,31 @@
 #include <string>
 
 #include <esp_err.h>
+#include <esp_event.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+
+
 class WiFi {
  public:
   WiFi();
   ~WiFi();
 
+  esp_err_t Inititialize();
   esp_err_t Connect(const std::string& ssid, const std::string& key);
+
+ private:
+static void EventHandler(void* arg,
+                   esp_event_base_t event_base,
+                   int32_t event_id,
+                   void* event_data);
+void EventHandler(
+                   esp_event_base_t event_base,
+                   int32_t event_id,
+                   void* event_data);
+
+  EventGroupHandle_t wifi_event_group_;
+  esp_event_handler_instance_t instance_any_id_;
+  esp_event_handler_instance_t instance_got_ip_;
+  int retry_num_;
 };
