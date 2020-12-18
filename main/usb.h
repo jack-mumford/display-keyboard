05509c93567f798be62ffe5119064c94b27d8a7f
Chris Mumford <cmumford@cmumford.com>
2020-12-18 08:32:13 -0800

    Added docs.

--- main/usb.h
+++ main/usb.h
@@ -9,16 +9,25 @@ class USB {
   USB();
   ~USB();
 
+  /**
+   * Is the device connected and configured.
+   */
   static bool Mounted();
+
+  /**
+   * Remote wake up host, only if suspended and enabled by host.
+   */
+  static esp_err_t RemoteWakup();
+
+  /**
+   * Check if device is suspended.
+   */
+  static bool Suspended();
 
   /**
    * Initialize the USB instance.
    */
   esp_err_t Initialize();
-
-  static esp_err_t RemoteWakup();
-
-  static bool Suspended();
 
   /**
    * Standard HID Boot Protocol Keyboard Report.
