7d9077a187637703ae7a6ddf6c64ffa203835687
Chris Mumford <cmumford@cmumford.com>
2020-12-18 18:26:01 -0800

    Added copyright notice.

--- main/hardware.c
+++ main/hardware.c
@@ -1,4 +1,29 @@
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 #include "hardware.h"
 
 #include <freertos/FreeRTOS.h>
@@ -19,6 +44,8 @@
 #include <tusb.h>
 
 // Copied from hw/bsp/esp32s2_saola_1/esp32s2_saola_1.c.
+const char TAG[] = "usb_hw";
+
 static void configure_pins(usb_hal_context_t* usb) {
   /* usb_periph_iopins currently configures USB_OTG as USB Device.
    * Introduce additional parameters in usb_hal_context_t when adding support
@@ -46,8 +73,8 @@ static void configure_pins(usb_hal_context_t* usb) {
   }
 }
 
-// Copied from hw/bsp/esp32s2_saola_1/esp32s2_saola_1.c.
 void board_init() {
+  ESP_LOGI(TAG, "board_init");
   // USB Controller Hal init
   periph_module_reset(PERIPH_USB_MODULE);
   periph_module_enable(PERIPH_USB_MODULE);
