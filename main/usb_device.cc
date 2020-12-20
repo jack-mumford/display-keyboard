3729341152b8569c450cb69279e78584e3d89f53
Chris Mumford <cmumford@cmumford.com>
2020-12-19 17:27:53 -0800

    Cleanup.

--- main/usb_device.cc
+++ main/usb_device.cc
@@ -14,11 +14,20 @@ namespace usb {
 
 namespace {
 
+struct AllDescriptors {
+  tusb_desc_configuration_t config_descriptor;
+  uint8_t hid_descriptor[sizeof(HID::kHIDDescriptor)];
+};
+
+// Make sure compiler didn't pack any space in between the members.
+static_assert(sizeof(AllDescriptors) ==
+              sizeof(tusb_desc_configuration_t) + sizeof(HID::kHIDDescriptor));
+
 constexpr char TAG[] = "kbd_usb";
 // TODO: These are from random.org. Need to get actual VID/PID numbers to
 //       avoid conflicts with other products.
-constexpr uint16_t kVendorID = 44699;
-constexpr uint16_t kProductID = 59002;
+constexpr uint16_t kVendorID = 0xae9b;
+constexpr uint16_t kProductID = 0xe67a;
 constexpr uint16_t kDeviceVersion = 0x0100;  // BCD.
 constexpr uint16_t kVersion = 0x0101;        // BCD.
 constexpr char kDeviceSerialNumber[] = "00001A";
@@ -50,21 +59,12 @@ constexpr tusb_desc_device_t kDeviceDescriptor = {
 constexpr tusb_desc_configuration_t kConfigurationDescriptor = {
     .bLength = sizeof(tusb_desc_configuration_t),
     .bDescriptorType = TUSB_DESC_CONFIGURATION,
-    .wTotalLength =
-        sizeof(tusb_desc_configuration_t) + sizeof(HID::kHIDDescriptor),
+    .wTotalLength = sizeof(AllDescriptors),
     .bNumInterfaces = 1,
     .bConfigurationValue = 1,
     .iConfiguration = 0x00,
     .bmAttributes = TU_BIT(7) | TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP,
     .bMaxPower = TUSB_DESC_CONFIG_POWER_MA(kMaxPower)};
-
-struct AllDescriptors {
-  tusb_desc_configuration_t config_descriptor;
-  uint8_t hid_descriptor[sizeof(HID::kHIDDescriptor)];
-};
-
-static_assert(sizeof(AllDescriptors) ==
-              sizeof(tusb_desc_configuration_t) + sizeof(HID::kHIDDescriptor));
 
 AllDescriptors g_descriptors;
 
@@ -73,14 +73,14 @@ extern "C" {
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
 uint8_t const* tud_descriptor_device_cb(void) {
-  return reinterpret_cast<uint8_t const*>(&g_descriptors);
+  return reinterpret_cast<uint8_t const*>(&kDeviceDescriptor);
 }
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor, whose contents must exist long
 // enough for transfer to complete
 uint8_t const* tud_descriptor_configuration_cb(uint8_t /*index*/) {
-  return reinterpret_cast<uint8_t const*>(&kConfigurationDescriptor);
+  return reinterpret_cast<uint8_t const*>(&g_descriptors);
 }
 
 // Invoked when received GET STRING DESCRIPTOR request
@@ -91,37 +91,38 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t /*index*/) {
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
   (void)langid;
 
+  // Pointer needs to outlive function call - hence static.
   constexpr uint8_t kMaxDescriptorLen = 32;
-  static uint16_t _desc_str[1 + kMaxDescriptorLen];
-  uint8_t chr_count;
+  static uint16_t _desc_str[1 + kMaxDescriptorLen];  // String header + string.
+  uint8_t num_chars;
 
   switch (index) {
     case STRID_LANGUAGE:
       _desc_str[1] = ((uint16_t)((uint32_t)kLanguage));
-      chr_count = 1;
+      num_chars = 1;
       break;
     case STRID_MANUFACTURER:
-      chr_count =
+      num_chars =
           strcpy_utf16(_desc_str + 1, kDeviceManufacturer, kMaxDescriptorLen);
       break;
     case STRID_PRODUCT:
-      chr_count = strcpy_utf16(_desc_str + 1, kProduct, kMaxDescriptorLen);
+      num_chars = strcpy_utf16(_desc_str + 1, kProduct, kMaxDescriptorLen);
       break;
     case STRID_SERIAL:
-      chr_count =
+      num_chars =
           strcpy_utf16(_desc_str + 1, kDeviceSerialNumber, kMaxDescriptorLen);
       break;
     case STRID_HID:
-      chr_count = strcpy_utf16(_desc_str + 1, HID::kStringDescriptor,
+      num_chars = strcpy_utf16(_desc_str + 1, HID::kStringDescriptor,
                                kMaxDescriptorLen);
       break;
     default:
-      chr_count = strcpy_utf16(_desc_str + 1, "<Unknown>", kMaxDescriptorLen);
+      num_chars = strcpy_utf16(_desc_str + 1, "<Unknown>", kMaxDescriptorLen);
       break;
   }
 
-  // first byte is length (including header), second byte is string type
-  _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
+  // Byte #1 = length. Byte #2 = string type.
+  _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 + 2 * num_chars);
 
   return _desc_str;
 }
@@ -142,6 +143,7 @@ esp_err_t Device::Initialize() {
   g_descriptors.config_descriptor = kConfigurationDescriptor;
   std::memcpy(g_descriptors.hid_descriptor, HID::kHIDDescriptor,
               sizeof(HID::kHIDDescriptor));
+  static_assert(HID::kHIDDescriptor[1] == TUSB_DESC_INTERFACE);
 
   board_init();
 
@@ -158,7 +160,6 @@ esp_err_t Device::RemoteWakup() {
 
 bool Device::Suspended() {
   return tud_suspended();
-  ;
 }
 
 void Device::Tick() {
