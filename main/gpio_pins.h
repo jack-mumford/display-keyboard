38cfb148a69a6ec255e8def8fe9adcd56d37137c
Chris Mumford <cmumford@cmumford.com>
2021-02-07 15:07:35 -0800

    Changed display pins for proto r2 assignments.

--- main/gpio_pins.h
+++ main/gpio_pins.h
@@ -11,15 +11,18 @@ constexpr gpio_num_t kActivityGPIO = GPIO_NUM_13;     // Pin for activity LED.
 constexpr gpio_num_t kI2C0SDA = GPIO_NUM_8;           // I2C port 0 SDA GPIO.
 constexpr gpio_num_t kI2C0SCL = GPIO_NUM_9;           // I2C port 0 SCL GPIO.
 constexpr gpio_num_t kKeyboardINTGPIO = GPIO_NUM_38;  // Keyboard event INT.
+constexpr gpio_num_t kI2C1SDA = GPIO_NUM_1;           // I2C port 1 SDA GPIO.
+constexpr gpio_num_t kI2C1SCL = GPIO_NUM_3;           // I2C port 1 SCL GPIO.
 
 /*
- * The SPI pins, used for display/touch, are specified in sdkconfig
- * SPI-CS-DISP  = 5
- * SPI-CS-TOUCH =
+ * The SPI pins, used for display/touch, are specified in sdkconfig.
+ *
+ * SPI-CS-DISP  = 7
+ * SPI-CS-TOUCH = 10
  * SPI-MOSI     = 35 (used for touch, but not display)
  * SPI-SCK      = 36
  * SPI-MISO     = 37
- * DC           = 6
+ * DC           = 5
  * Reset        = 0 (TODO: verify this)
  */
 
