9c4c3be7279c0dec4631a702e97f7c8f2e7e05e0
Chris Mumford <cmumford@cmumford.com>
2021-06-13 11:26:29 -0700

    Using array for keyboard config.

--- scripts/genmap.py
+++ scripts/genmap.py
@@ -18,14 +18,17 @@ vars.extend(['LOGIC_1', 'LOGIC_2'])
 vars.extend(['UNUSED_%d' % i for i in range(9)])
 vars.append('UNLOCK_WILDCARD')
 
+print('enum EventID {')
 idx = 0
 for var in vars:
     if type(var) is tuple:
-        print("%s = %d, // %s" % (var[0], idx, var[1]))
+        print("  %s = %d, // %s" % (var[0], idx, var[1]))
     else:
-        print("%s = %d," % (var, idx))
+        print("  %s = %d," % (var, idx))
     idx += 1
+print('};')
 
+print('switch (event_id) {')
 idx = 0
 for var in vars:
     if type(var) is tuple:
@@ -33,3 +36,32 @@ for var in vars:
     else:
         print("    case %d: return \"%s\";" % (idx, var))
     idx += 1
+print('}')
+
+key_map = {
+    'R0_C3': 'HID_KEY_SHIFT_LEFT',
+    'R0_C2': 'HID_KEY_Q',
+    'R0_C1': 'HID_KEY_W',
+    'R0_C0': 'HID_KEY_E',
+    'R1_C3': 'HID_KEY_CONTROL_LEFT',
+    'R1_C2': 'HID_KEY_A',
+    'R1_C1': 'HID_KEY_S',
+    'R1_Co': 'HID_KEY_D',
+}
+print()
+print()
+print('constexpr std::array<uint8_t, %d> kEventID2KeyCode = {' % len(vars))
+event_id = 0
+for v in vars:
+    if type(v) is tuple:
+        var = v[0]
+    else:
+        var = v
+    key_code = 0
+    if var in key_map:
+        key_code = key_map[var]
+    else:
+        key_code = 'HID_KEY_NONE'
+    print("  %s, // %d: %s" % (key_code, event_id, var))
+    event_id += 1
+print('};')
